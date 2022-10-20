843cc1f623737e42285f0b0b462a8ae57ad48181
markingcarlos <carlos.a.souza@unesp.br>
2022-10-19 23:12:50 -0300

    Sending Project

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(
+  ".cc-bg svg > g g:nth-child(1) > path"
+)
+const ccBgColor02 = document.querySelector(
+  ".cc-bg svg > g g:nth-child(2) > path"
+)
+
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+function setCardType(type) {
+  const colors = {
+    visa: ["white", "blue"],
+    mastercard: ["orange", "red"],
+    default: ["black", "white"],
+  }
+
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+
+setCardType("mastercard")
